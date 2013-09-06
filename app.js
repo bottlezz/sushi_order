094c52d0e24e6651b06447b39257cabf6a094771
Gregory Zhou <bottlezz2002@gmail.com>
2013-09-06 15:04:51 -0700

    One Order PerUser

--- app.js
+++ app.js
@@ -44,33 +44,33 @@ app.get('/users', user.list);
 app.post("/submit" ,function(req,res){
 	var user = req.body.user;
 	var order = req.body.order;
-  console.log("asdf");
-	/*if(user && score){
-		var scores = storage.getItem('scores');
-		scores.push({'user':user,'score':score});
-		scores.sort(function(a,b){
-			return b.score - a.score;
-		});
-		console.log(scores);
-		storage.setItem('scores',scores);
-		res.json(scores);
-	}else{
-		res.send("BAD");
-	}*/
+  
+
   var orders=storage.getItem('orders');
-  orders.push({user:user,order:order});
+  
+  var index=-1;
+  for(var i=0;i<orders.length;i++){
+    if(orders[i].user==user)
+      index=i;
+  }
+  if(index==-1)
+    orders.push({user:user,order:order});
+  else
+    orders[index]={user:user,order:order};
   storage.setItem('orders',orders);
   //res.json(orders);
   io.sockets.emit('update',orders);
 });
 
-// show scores
+
 app.get("/orders" ,function(req,res){
 	var orders = storage.getItem('orders');
 	res.json(orders);
 });
+
 app.get("/clearOrders",function(req,res){
   storage.setItem('orders',[]);
+  routes.index(req,res);
 });
 
 var server=http.createServer(app).listen(app.get('port'), function(){
